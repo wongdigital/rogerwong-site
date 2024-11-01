af050709cb5c6ff91065849e9f01a849c3354e92
wongdigital <roger@wong.digital>
2024-10-31 23:41:25 -0700

    AVP blog post

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['example.com', 'images.unsplash.com','vercel.app','rogerwong-site-3q85lbw2r-roger-wongs-projects.vercel.app'], // Add any external domains you're using
+    domains: ['rogerwong-site.vercel.app', 'images.unsplash.com','vercel.app'], // Add any external domains you're using
     remotePatterns: [
       {
         protocol: 'https',
@@ -13,3 +13,15 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/thoughts-on-apple-vision-pro',
+        destination: '/posts/thoughts-on-apple-vision-pro',
+        permanent: true,
+      },
+    ]
+  },
+}
