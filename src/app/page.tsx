55752633e728ce05c61efa25a90ed1be8e2d1961
wongdigital <roger@wong.digital>
2024-11-10 09:46:40 -0800

    Updated homepage to include more posts and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link'
 
 export default async function Home() {
   const allPostsData = await getSortedPostsData();
-  const latestPost = allPostsData[0]; // Get the most recent post
+  const latestPosts = allPostsData.slice(0, 3); // Get the three most recent posts
   const sortedLinks = getSortedLinklogData();
   
   return (
@@ -18,22 +18,29 @@ export default async function Home() {
       <section className="my-8 py-8">
         <div className="flex flex-col md:flex-row gap-8">
           <div className="w-full lg:w-7/12">
-            <h3 className="text-sm font-extrabold mb-4 text-slate-500">Latest Post</h3>
-            <PostPreview
-              title={latestPost.title}
-              date={latestPost.date}
-              readTime={calculateReadTime(latestPost.content)}
-              imageSrc={latestPost.imageSrc}
-              imageAlt={latestPost.imageAlt}
-              excerpt={latestPost.excerpt}
-              slug={latestPost.id}
-            />
+            <h3 className="text-sm font-extrabold mb-4 text-slate-500">Latest Posts</h3>
+            <div className="space-y-8 mb-4">
+              {latestPosts.map((post) => (
+                <PostPreview
+                  key={post.id}
+                  title={post.title}
+                  date={post.date}
+                  readTime={calculateReadTime(post.content)}
+                  imageSrc={post.imageSrc}
+                  imageAlt={post.imageAlt}
+                  excerpt={post.excerpt}
+                  slug={post.id}
+                />
+              ))}
+            </div>
+            <div className="mt-8">
             <Link className="text-blue-600 dark:text-blue-500 hover:underline hover:text-blue-500 dark:hover:text-blue-400" href="/posts">More Posts…</Link>
+            </div>
           </div>
           <div className="w-full lg:w-5/12">
             <h3 className="text-sm font-extrabold mb-4 text-slate-500">Latest Links</h3>
             <div className="mb-4">
-              {sortedLinks.slice(0, 5).map((link, index) => (
+              {sortedLinks.slice(0, 6).map((link, index) => (
                 <LinklogPreview
                   key={index}
                   title={link.title}
