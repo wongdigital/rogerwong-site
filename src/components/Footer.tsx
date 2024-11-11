94c89ec2999e07070b2ccf614c7d01eea8ccaa11
wongdigital <roger@wong.digital>
2024-11-11 10:43:46 -0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,26 @@
 import Link from 'next/link';
+import { RssIcon } from '@heroicons/react/24/outline';
 
 export default function Footer() {
   return (
     <div className="py-4 mt-8 flex flex-col md:flex-row justify-between items-center text-sm text-slate-500 dark:text-slate-400">
       <div>
-        © {new Date().getFullYear()} Roger Wong. All rights reserved.
+        © 1995&ndash;{new Date().getFullYear()} Roger Wong. All rights reserved.
       </div>
-      <div className="flex gap-4 mt-4 md:mt-0">
+      <div className="flex gap-6 mt-4 md:mt-0">
         <Link 
           href="/rss.xml"
-          className="hover:text-slate-600 dark:hover:text-slate-300 hover:underline"
+          className="hover:text-slate-600 dark:hover:text-slate-300 hover:underline flex items-center gap-1"
           aria-label="RSS feed for blog posts"
         >
-          Posts RSS
+          <RssIcon className="w-4 h-4" />Posts RSS
         </Link>
         <Link 
           href="/linklog.xml"
-          className="hover:text-slate-600 dark:hover:text-slate-300 hover:underline"
+          className="hover:text-slate-600 dark:hover:text-slate-300 hover:underline flex items-center gap-1"
           aria-label="RSS feed for linklog"
         >
-          Links RSS
+          <RssIcon className="w-4 h-4" />Links RSS
         </Link>
       </div>
     </div>
